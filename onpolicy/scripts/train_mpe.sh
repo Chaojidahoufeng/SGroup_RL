6b27f25b371cc094c09e434877233fcbff4ce573
Yuzi Yan <yanyuzithu@163.com>
2021-05-29 10:34:34 +0800

    add some code from Mac

--- onpolicy/scripts/train_mpe.sh
+++ onpolicy/scripts/train_mpe.sh
@@ -12,21 +12,22 @@ for seed in `seq ${seed_max}`;
 do
 let "seed=$seed+1"
 echo "seed is ${seed}:"
-srun -p gpu -N 1 -n 1 python -u train/train_mpe.py --env_name ${env} \ 
---algorithm_name ${algo} \ 
---experiment_name ${exp} \ 
---scenario_name ${scenario} \ 
---num_agents ${num_agents} \ 
---num_landmarks ${num_landmarks} \ 
---seed 50 \ 
---n_training_threads 1 \ 
---n_rollout_threads 1 \ 
---num_mini_batch 1 \ 
---episode_length 250 \ 
---num_env_steps 20000000 \ 
---ppo_epoch 10 \ 
---gain 0.01 \ 
---lr 7e-4 \ 
---critic_lr 7e-4 \ 
+srun -p gpu -N 1 -n 1 python -u train/train_mpe.py \
+--env_name ${env} \
+--algorithm_name ${algo} \
+--experiment_name ${exp} \
+--scenario_name ${scenario} \
+--num_agents ${num_agents} \
+--num_landmarks ${num_landmarks} \
+--seed 50 \
+--n_training_threads 1 \
+--n_rollout_threads 1 \
+--num_mini_batch 1 \
+--episode_length 250 \
+--num_env_steps 20000000 \
+--ppo_epoch 10 \
+--gain 0.01 \
+--lr 7e-4 \
+--critic_lr 7e-4 \
 --user_name "mapping"
 done
