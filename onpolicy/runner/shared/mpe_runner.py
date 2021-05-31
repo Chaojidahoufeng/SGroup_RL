e0c079d9b037d6511cb730d5cffd09e6b3434cd5
Yuzi Yan <yanyuzithu@163.com>
2021-05-31 14:47:59 +0800

    add some code from Mac

--- onpolicy/runner/shared/mpe_runner.py
+++ onpolicy/runner/shared/mpe_runner.py
@@ -17,6 +17,8 @@ class MPERunner(Runner):
         super(MPERunner, self).__init__(config)
 
     def run(self):
+        import pdb
+        pdb.set_trace()
         self.warmup()   
 
         start = time.time()
