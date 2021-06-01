5bced4f4df8ee1903c905b40fe93002a095b8abe
Yuzi Yan <yanyuzithu@163.com>
2021-06-01 11:09:06 +0800

    add some code from Mac

--- onpolicy/scripts/render_mpe.sh
+++ onpolicy/scripts/render_mpe.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 env="MPE"
-scenario="simple_spread"
-num_landmarks=3
-num_agents=3
+scenario="rel_formation_form_error"
+num_landmarks=1
+num_agents=4
 algo="rmappo"
-exp="render"
+exp="5-28-rel-formation-form-error-comm-avoid-5-form-0_05-dist-0"
 seed_max=1
 
 echo "env is ${env}"
 for seed in `seq ${seed_max}`
 do
-    CUDA_VISIBLE_DEVICES=4 python render/render_mpe.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_training_threads 1 --n_rollout_threads 1 --use_render --episode_length 25 --render_episodes 5 --model_dir "/home/yuchao/project/onpolicy/onpolicy/scripts/results/MPE/simple_spread/rmappo/newcode/wandb/run-20201130_161745-2tagw0g7/files/"
+    CUDA_VISIBLE_DEVICES=4 python render/render_mpe.py \
+    --env_name ${env} \
+    --algorithm_name ${algo} \
+    --experiment_name ${exp} \
+    --scenario_name ${scenario} \
+    --num_agents ${num_agents} \
+    --num_landmarks ${num_landmarks} \
+    --seed ${seed} \
+    --n_training_threads 1 \
+    --n_rollout_threads 1 \
+    --use_render \
+    --episode_length 250 
+    --render_episodes 5 \
+    --model_dir "/home/yuchao/project/onpolicy/onpolicy/scripts/results/MPE/simple_spread/rmappo/newcode/wandb/run-20201130_161745-2tagw0g7/files/"
 done
