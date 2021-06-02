b67cdfb4bfb9950bd7518b00b06b1823a953d5ee
Yuzi Yan <yanyuzithu@163.com>
2021-06-02 16:17:58 +0800

    add some code from Mac

--- onpolicy/algorithms/utils/distributions.py
+++ onpolicy/algorithms/utils/distributions.py
@@ -64,7 +64,7 @@ class SoftCategorical(nn.Module):
             return init(m, init_method, lambda x: nn.init.constant_(x, 0), gain)
 
         self.linear = init_(nn.Linear(num_inputs, num_outputs))
-        self.softmax_layer = nn.Softmax(num_outputs)
+        self.softmax_layer = nn.Softmax(num_outputs, dim=1)
 
     def forward(self, x, available_actions=None):
         import pdb
