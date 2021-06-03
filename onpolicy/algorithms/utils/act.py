754db25ba41695679d8ad559a3af17d89d986620
Yuzi Yan <yanyuzithu@163.com>
2021-06-03 13:48:37 +0800

    add some code from Mac

--- onpolicy/algorithms/utils/act.py
+++ onpolicy/algorithms/utils/act.py
@@ -25,7 +25,7 @@ class ACTLayer(nn.Module):
 
         if action_space.__class__.__name__ == "Discrete":
             action_dim = action_space.n
-            self.action_out = Categorical(inputs_dim, action_dim, use_orthogonal, gain)
+            self.action_out = SoftCategorical(inputs_dim, action_dim, use_orthogonal, gain)
         elif action_space.__class__.__name__ == "Box":
             self.continuous_action = True
             action_dim = action_space.shape[0]
