58d45fd4f8b09e6628db3b14e006f48f766f89d8
Yuzi Yan <yanyuzithu@163.com>
2021-06-05 15:47:30 +0800

    add some code from Mac

--- onpolicy/envs/mpe/rendering.py
+++ onpolicy/envs/mpe/rendering.py
@@ -41,7 +41,7 @@ def get_display(spec):
     else:
         raise error.Error('Invalid display specification: {}. (Must be a string like :0 or None.)'.format(spec))
 
-class Viewer(object):
+class Viewer_origin(object):
     def __init__(self, width, height, display=None):
         display = get_display(display)
 
@@ -381,7 +381,7 @@ def make_text(text='', font_size=36, x=None, y=None, anchor_x='center', anchor_y
 
 
 '''
-make_square, make_triangle
+make_square, make_triangle, Viewer
 Author: Yuzi Yan
 Date: 2021.6.5
 '''
@@ -405,3 +405,115 @@ def make_square(radius=10, res=4, angle=0, filled=True):
         return FilledPolygon(points)
     else:
         return PolyLine(points, True)
+
+class Viewer(object):
+    def __init__(self, width, height, display=None):
+        self.width = width
+        self.height = height
+        display = get_display(display)
+
+        self.window = pyglet.window.Window(width=width, height=height, display=display)
+        self.on_close = self.window_closed_by_user
+        self.geoms = []
+        self.onetime_geoms = []
+        self.labels = []
+        self.onetime_labels = []
+        self.transform = Transform()
+
+        glEnable(GL_BLEND)
+        # glEnable(GL_MULTISAMPLE)
+        glEnable(GL_LINE_SMOOTH)
+        # glHint(GL_LINE_SMOOTH_HINT, GL_DONT_CARE)
+        glHint(GL_LINE_SMOOTH_HINT, GL_NICEST)
+        glLineWidth(2.0)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+    def close(self):
+        self.window.close()
+
+    def window_closed_by_user(self):
+        self.close()
+
+    def set_bounds(self, left, right, bottom, top):
+        assert right > left and top > bottom
+        scalex = self.width/(right-left)
+        scaley = self.height/(top-bottom)
+        self.transform = Transform(
+            translation=(-left*scalex, -bottom*scaley),
+            scale=(scalex, scaley))
+
+    def add_geom(self, geom):
+        self.geoms.append(geom)
+
+    def add_onetime(self, geom):
+        self.onetime_geoms.append(geom)
+
+    def add_label(self, label):
+        self.labels.append(label)
+
+    def render(self, return_rgb_array=False):
+        glClearColor(1,1,1,1)
+
+        self.window.clear()
+        self.window.switch_to()
+        self.window.dispatch_events()
+        self.transform.enable()
+        for geom in self.geoms:
+            geom.render()
+        for geom in self.onetime_geoms:
+            geom.render()
+        for label in self.labels:
+            label.render()
+        self.transform.disable()
+
+
+        arr = None
+        if return_rgb_array:
+            buffer = pyglet.image.get_buffer_manager().get_color_buffer()
+            image_data = buffer.get_image_data()
+            arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
+            # In https://github.com/openai/gym-http-api/issues/2, we
+            # discovered that someone using Xmonad on Arch was having
+            # a window of size 598 x 398, though a 600 x 400 window
+            # was requested. (Guess Xmonad was preserving a pixel for
+            # the boundary.) So we use the buffer height/width rather
+            # than the requested one.
+            arr = arr.reshape(buffer.height, buffer.width, 4)
+            arr = arr[::-1,:,0:3]
+        self.window.flip()
+        self.onetime_geoms = []
+
+        return arr
+
+    # Convenience
+    def draw_circle(self, radius=10, res=30, filled=True, **attrs):
+        geom = make_circle(radius=radius, res=res, filled=filled)
+        _add_attrs(geom, attrs)
+        self.add_onetime(geom)
+        return geom
+
+    def draw_polygon(self, v, filled=True, **attrs):
+        geom = make_polygon(v=v, filled=filled)
+        _add_attrs(geom, attrs)
+        self.add_onetime(geom)
+        return geom
+
+    def draw_polyline(self, v, **attrs):
+        geom = make_polyline(v=v)
+        _add_attrs(geom, attrs)
+        self.add_onetime(geom)
+        return geom
+
+    def draw_line(self, start, end, **attrs):
+        geom = Line(start, end)
+        _add_attrs(geom, attrs)
+        self.add_onetime(geom)
+        return geom
+
+    def get_array(self):
+        self.window.flip()
+        image_data = pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
+        self.window.flip()
+        arr = np.fromstring(image_data.data, dtype=np.uint8, sep='')
+        arr = arr.reshape(self.height, self.width, 4)
+        return arr[::-1,:,0:3]
