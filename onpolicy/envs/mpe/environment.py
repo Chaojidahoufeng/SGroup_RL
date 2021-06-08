c72eb521fa296874930043dceddcbf4ae60bc1a9
Yuzi Yan <yanyuzithu@163.com>
2021-06-08 17:36:41 +0800

    add some code from Mac

--- onpolicy/envs/mpe/environment.py
+++ onpolicy/envs/mpe/environment.py
@@ -40,7 +40,7 @@ class MultiAgentEnv(gym.Env):
         self.discrete_action_space = discrete_action
 
         # if true, action is a number 0...N, otherwise action is a one-hot N-dimensional vector
-        self.discrete_action_input = True
+        self.discrete_action_input = False
         # if true, even the action is continuous, action will be performed discretely
         self.force_discrete_action = world.discrete_action if hasattr(
             world, 'discrete_action') else False
