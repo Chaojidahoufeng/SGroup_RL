1ec176ffca60e30236f6d1e5a49f76eec2a8937b
Yuzi Yan <yanyuzithu@163.com>
2021-06-09 14:21:23 +0800

    add some code from Mac

--- onpolicy/envs/mpe/environment_11_discrete_action.py
+++ onpolicy/envs/mpe/environment_11_discrete_action.py
@@ -394,7 +394,7 @@ class MultiAgentEnv(gym.Env):
         for i in range(len(self.viewers)):
             # create viewers (if necessary)
             WINDOW_W = 700
-            WINDOW_H = 700
+            WINDOW_H = 350
             if self.viewers[i] is None:
                 # import rendering only if we need it (and don't import for headless machines)
                 #from gym.envs.classic_control import rendering
