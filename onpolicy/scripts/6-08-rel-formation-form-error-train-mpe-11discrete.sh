d22ee77d524b2174340a5424d62125e6f55be699
Yuzi Yan <yanyuzithu@163.com>
2021-06-09 14:01:35 +0800

    add some code from Mac

--- onpolicy/scripts/6-08-rel-formation-form-error-train-mpe-11discrete.sh
+++ onpolicy/scripts/6-08-rel-formation-form-error-train-mpe-11discrete.sh
@@ -24,7 +24,7 @@ do
     --n_rollout_threads 32 \
     --num_mini_batch 1 \
     --episode_length 250 \
-    --num_env_steps 50000000 \
+    --num_env_steps 500000000 \
     --ppo_epoch 10 \
     --gain 0.01 \
     --lr 7e-4 \
