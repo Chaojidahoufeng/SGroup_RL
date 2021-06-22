213267533e2d36d06a78c7d394c785edcfb45416
Yuzi Yan <yanyuzithu@163.com>
2021-06-22 15:04:48 +0800

    add some code from Mac

--- autoSyn_local.sh
+++ autoSyn_local.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git status  
 git add *  
-git commit -m 'add some code from Mac'
+git commit -am 'add some code from Mac'
 # git commit -m 'add some results from Server'
 git pull --rebase origin main   #domnload data
 git push origin main            #upload data
