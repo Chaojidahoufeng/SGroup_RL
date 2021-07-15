5eae030e5bb91902c07f7a7150eeafe6dd6bf4a8
Yuzi Yan <yanyuzithu@163.com>
2021-07-15 10:46:17 +0800

    add some code from Mac

--- onpolicy/scripts/train_mve_7_14_form_crash.sh
+++ onpolicy/scripts/train_mve_7_14_form_crash.sh
@@ -16,7 +16,7 @@ do
     --algorithm_name ${algo} --experiment_name ${exp} --scenario_name ${scenario} \
     --user_name "yuzi" --num_agents ${num_agents} --num_landmarks ${num_landmarks} --num_obstacles ${num_obstacles}\
     --seed ${seed} --n_training_threads 32 --n_rollout_threads 2 --num_mini_batch 1 \
-    --episode_length 25 --num_env_steps 5000000 --ppo_epoch 10 --gain 0.01 \
+    --episode_length 25 --num_env_steps 500000 --ppo_epoch 10 --gain 0.01 \
     --lr 7e-4 --critic_lr 7e-4 --use_recurrent_policy \
     --direction_alpha 0.1 
     #ß--wandb_name "tartrl"
