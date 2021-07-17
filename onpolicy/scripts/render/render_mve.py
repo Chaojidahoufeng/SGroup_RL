53f1afb6d6ac2520785f657e36d62c97b895b2df
Yuzi Yan <yanyuzithu@163.com>
2021-07-17 10:51:28 +0800

    add some code from Mac

--- onpolicy/scripts/render/render_mve.py
+++ onpolicy/scripts/render/render_mve.py
@@ -51,17 +51,21 @@ def make_render_env(all_args):
 
 def parse_args(args, parser):
     parser.add_argument('--scenario_name', type=str,
-                        default='3p2t2f', help="Which scenario to run on")
-    parser.add_argument("--num_landmarks", type=int, default=3)
+                        default='3p1t2f', help="Which scenario to run on")
+    parser.add_argument("--num_landmarks", type=int, default=0)
     parser.add_argument('--num_agents', type=int,
-                        default=3, help="number of players")
+                        default=4, help="number of players")
+    parser.add_argument('--num_obstacles', type=int, default=0)
     parser.add_argument('--guiport',type=str,default='/dev/shm/gui_port')
     parser.add_argument('--usegui', action='store_true', default=False)
     parser.add_argument('--step-t',type=float,default=1.0)
     parser.add_argument('--sim-step',type=int,default=100)
-    parser.add_argument('--direction_alpha', type=float, default=1.0)
+    parser.add_argument('--direction_alpha', type=float, default=0.1)
     parser.add_argument('--add_direction_encoder',type=str, default='train')
+
+    parser.add_argument('--ideal_side_len', type=int, default=0.75)
     all_args = parser.parse_known_args(args)[0]
+
     return all_args
 
 
