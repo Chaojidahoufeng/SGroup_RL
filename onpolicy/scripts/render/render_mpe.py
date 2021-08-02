2ac39f09a82f5211a77f1daf89b7128802662ae8
Yuzi Yan <yanyuzithu@163.com>
2021-08-02 16:54:15 +0800

    add some code from Mac

--- onpolicy/scripts/render/render_mpe.py
+++ onpolicy/scripts/render/render_mpe.py
@@ -49,6 +49,8 @@ def parse_args(args, parser):
     parser.add_argument("--dist-rew-weight", type=float, default=0.002)
     parser.add_argument("--action_space_dim",  type=int, default=4)
 
+    parser.add_argument("--use_11_discrete_action", action="store_true", default=False)
+
     all_args = parser.parse_known_args(args)[0]
 
     return all_args
