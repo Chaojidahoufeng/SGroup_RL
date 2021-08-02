f9a180ea79efbe2632846f616e96ae24a2925b51
Yuzi Yan <yanyuzithu@163.com>
2021-08-02 14:38:07 +0800

    add some code from Mac

--- onpolicy/envs/mpe/core_alpha.py
+++ onpolicy/envs/mpe/core_alpha.py
@@ -272,6 +272,8 @@ class World(object):
 
     def integrate_state(self, p_force):
         for i, entity in enumerate(self.entities):
+            if i == 0:
+                print(entity.state.p_vel)
             if not entity.movable:
                 continue
             entity.state.p_vel = entity.state.p_vel * (1 - self.damping)
