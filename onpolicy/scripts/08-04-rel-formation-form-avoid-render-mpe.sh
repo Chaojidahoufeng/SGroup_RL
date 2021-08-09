2ee61d372d3b0b79a09b373e6e7b1582c7fb2209
Yuzi Yan <yanyuzithu@163.com>
2021-08-09 10:05:22 +0800

    add some code from Mac

--- onpolicy/scripts/08-04-rel-formation-form-avoid-render-mpe.sh
+++ onpolicy/scripts/08-04-rel-formation-form-avoid-render-mpe.sh
@@ -4,7 +4,7 @@ scenario="rel_formation_form_error"
 num_landmarks=1
 num_agents=4
 algo="rmappo"
-exp="08-04-rel-formation-avoid-5-form-0_05"
+exp="08-04-rel-formation-avoid-5-form-0_05-render"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
