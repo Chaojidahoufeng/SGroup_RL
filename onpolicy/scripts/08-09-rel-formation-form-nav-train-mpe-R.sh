21a0922323f8e610e2caacd5d03442ccaf7ced65
Yuzi Yan <yanyuzithu@163.com>
2021-08-09 23:57:31 +0800

    add some code from Mac

--- onpolicy/scripts/08-09-rel-formation-form-nav-train-mpe-R.sh
+++ onpolicy/scripts/08-09-rel-formation-form-nav-train-mpe-R.sh
@@ -4,7 +4,7 @@ scenario="rel_formation_form_error"
 num_landmarks=1
 num_agents=4
 algo="rmappo"
-exp="08-09-rel-formation-form-nav-train-mpe"
+exp="08-09-rel-formation-form-nav-train-mpe-R"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
@@ -12,26 +12,25 @@ for seed in `seq ${seed_max}`;
 do
     let "seed=$seed+1"
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_mpe.py \
+    CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
     --env_name ${env} \
     --algorithm_name ${algo} \
     --experiment_name ${exp} \
     --scenario_name ${scenario} \
     --num_agents ${num_agents} \
     --num_landmarks ${num_landmarks} \
-    --seed 50 \
-    --n_training_threads 4 \
-    --n_rollout_threads 32 \
-    --num_mini_batch 1 \
-    --episode_length 250 \
-    --num_env_steps 25000000 \
-    --ppo_epoch 10 \
-    --gain 0.01 \
-    --lr 7e-4 \
-    --critic_lr 7e-4 \
-    --user_name "mapping" \
+    --seed ${seed} \
+    --n_training_threads 1 \
+    --n_rollout_threads 1 \
     --avoid-rew-weight 5 \
     --form-rew-weight 0.05 \
     --nav-rew-weight 5 \
-    --num_static_obs 0
+    --num_static_obs 5 \
+    --use_render \
+    --episode_length 250 \
+    --render_episodes 1 \
+    --num_static_obs 0 \
+    --model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-09-rel-formation-form-nav-train-mpe/run1/models" \
+    --save_gifs \
+    --eval_interval 250
 done
