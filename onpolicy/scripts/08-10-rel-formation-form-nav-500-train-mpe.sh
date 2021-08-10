85e60608d2b38d99b87e2c0312ebb9dd1ce1ce76
Yuzi Yan <yanyuzithu@163.com>
2021-08-10 10:26:02 +0800

    add some code from Mac

--- onpolicy/scripts/08-10-rel-formation-form-nav-500-train-mpe.sh
+++ onpolicy/scripts/08-10-rel-formation-form-nav-500-train-mpe.sh
@@ -4,7 +4,7 @@ scenario="rel_formation_form_error"
 num_landmarks=1
 num_agents=4
 algo="rmappo"
-exp="08-19-rel-formation-form-nav-500-train-mpe"
+exp="08-10-rel-formation-form-nav-500-train-mpe"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
