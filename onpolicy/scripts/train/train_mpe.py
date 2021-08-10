3b64043347e8d7308f5282253bd0ee44b78b6cfe
Yuzi Yan <yanyuzithu@163.com>
2021-08-10 11:55:24 +0800

    add some code from Mac

--- onpolicy/scripts/train/train_mpe.py
+++ onpolicy/scripts/train/train_mpe.py
@@ -164,6 +164,8 @@ def main(args):
     }
 
     # run experiments
+    import pdb
+    pdb.set_trace()
     if all_args.share_policy:
         from onpolicy.runner.shared.mpe_runner import MPERunner as Runner
     else:
