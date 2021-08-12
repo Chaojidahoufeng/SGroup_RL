c58616be621482c23595b42fe5bc02403d64242d
Yuzi Yan <yanyuzithu@163.com>
2021-08-12 23:30:37 +0800

    add some code from Mac

--- onpolicy/scripts/08-12-rel-formation-form-nav10-avoid-train-mpe.sh
+++ onpolicy/scripts/08-12-rel-formation-form-nav10-avoid-train-mpe.sh
@@ -4,7 +4,7 @@ scenario="rel_formation_form_error"
 num_landmarks=1
 num_agents=4
 algo="rmappo"
-exp="08-12-rel-formation-form-nav10-train-mpe"
+exp="08-12-rel-formation-form-nav10-avoid-train-mpe"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
