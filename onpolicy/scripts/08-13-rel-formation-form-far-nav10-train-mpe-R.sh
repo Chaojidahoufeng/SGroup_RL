f12fde3b91b976c20543a4ef6b74dc0782a5ec73
Yuzi Yan <yanyuzithu@163.com>
2021-08-13 16:23:21 +0800

    add some code from Mac

--- onpolicy/scripts/08-13-rel-formation-form-far-nav10-train-mpe-R.sh
+++ onpolicy/scripts/08-13-rel-formation-form-far-nav10-train-mpe-R.sh
@@ -30,7 +30,7 @@ do
     --episode_length 250 \
     --render_episodes 5 \
     --num_static_obs 0 \
-    --model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error_far/rmappo/08-13-rel-formation-form-far-nav10-train-mpe/run1/models" \
+    --model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error_far/rmappo/08-13-rel-formation-form-far-nav10-train-mpe/run2/models" \
     --save_gifs \
     --eval_interval 250 \
     --map-max-size 12000
