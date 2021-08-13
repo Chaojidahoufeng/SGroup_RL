37f0e789624f4892e913a22ce45018d0f8eb0caa
Yuzi Yan <yanyuzithu@163.com>
2021-08-13 16:01:52 +0800

    add some code from Mac

--- onpolicy/envs/mpe/environment_alpha.py
+++ onpolicy/envs/mpe/environment_alpha.py
@@ -456,8 +456,8 @@ class MultiAgentEnv(gym.Env):
                 #                                         color=(0, 0, 0, 255))
                 #     self.viewers[i].add_label(agent_reward_text)
             time = rendering.make_text(text='time = %f sec' % self.world.time, font_size=15,
-                                           x=agents_pos_center[0] // 2 - WINDOW_W // 1.5,
-                                           y=agents_pos_center[1] // 2 - WINDOW_H // 2.0,
+                                           x=agents_pos_center[0] // 2,
+                                           y=agents_pos_center[1] // 2,
                                            anchor_x='left',
                                            color=(0, 0, 0, 255))
             distance = rendering.make_text(text='distance = %f meters' % self.world.distance, font_size=15,
