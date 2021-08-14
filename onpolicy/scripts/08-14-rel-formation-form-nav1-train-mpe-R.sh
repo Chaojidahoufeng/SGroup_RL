63ad9c333338d256b02ec7d6c7cde958db939442
Yuzi Yan <yanyuzithu@163.com>
2021-08-14 10:31:20 +0800

    add some code from Mac

--- onpolicy/scripts/08-14-rel-formation-form-nav1-train-mpe-R.sh
+++ onpolicy/scripts/08-14-rel-formation-form-nav1-train-mpe-R.sh
@@ -4,7 +4,7 @@ scenario="rel_formation_form_error"
 num_landmarks=1
 num_agents=4
 algo="rmappo"
-exp="08-13-rel-formation-form-nav10-10avoid-train-mpe-R"
+exp="08-14-rel-formation-form-nav1-train-mpe-R"
 seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
@@ -24,14 +24,14 @@ do
     --n_rollout_threads 1 \
     --avoid-rew-weight 5 \
     --form-rew-weight 0.05 \
-    --nav-rew-weight 10 \
+    --nav-rew-weight 1 \
     --num_static_obs 10 \
     --use_render \
     --episode_length 250 \
     --render_episodes 5 \
-    --num_static_obs 10 \
-    --model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-13-rel-formation-form-nav10-10avoid-train-mpe/run1/models" \
+    --num_static_obs 0 \
+    --model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-14-rel-formation-form-nav1-train-mpe/run1/models" \
     --save_gifs \
     --eval_interval 250 \
-    --map-max-size 1200
+    --map-max-size 3600
 done
