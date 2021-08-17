65977c7119ef88b4839714bd5c3872b180b58a83
Yuzi Yan <yanyuzithu@163.com>
2021-08-17 09:16:23 +0800

    add some code from Mac

---
+++ onpolicy/scripts/08-17-rel-formation-form-selfnav10-train-mpe-obs0-trapezoid-share.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+env="MPE"
+scenario="rel_formation_form_error" 
+num_landmarks=1
+num_agents=4
+algo="rmappo"
+exp="08-17-rel-formation-form-selfnav10-train-mpe-obs0-trapezoid-seperate"
+seed_max=1
+
+echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
+for seed in `seq ${seed_max}`;
+do
+    let "seed=$seed+1"
+    echo "seed is ${seed}:"
+    CUDA_VISIBLE_DEVICES=1 python train/train_mpe.py \
+    --env_name ${env} \
+    --algorithm_name ${algo} \
+    --experiment_name ${exp} \
+    --scenario_name ${scenario} \
+    --num_agents ${num_agents} \
+    --num_landmarks ${num_landmarks} \
+    --seed 50 \
+    --n_training_threads 4 \
+    --n_rollout_threads 32 \
+    --num_mini_batch 1 \
+    --episode_length 250 \
+    --num_env_steps 20000000 \
+    --ppo_epoch 10 \
+    --gain 0.01 \
+    --lr 7e-4 \
+    --critic_lr 7e-4 \
+    --user_name "mapping" \
+    --avoid-rew-weight 5 \
+    --form-rew-weight 0.05 \
+    --nav-rew-weight 0 \
+    --self-nav-rew-weight 10 \
+    --num_static_obs 0 \
+    --map-max-size 3600 \
+    --static_obs_intensity 0e-6 \
+    --topo_type 'trapezoid'
+done
