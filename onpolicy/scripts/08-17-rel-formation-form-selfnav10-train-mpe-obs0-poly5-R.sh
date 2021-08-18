3e6d9fbb9a0578d2cca3edfa4e9c92bdc178d6d8
Yuzi Yan <yanyuzithu@163.com>
2021-08-19 00:25:34 +0800

    add some code from Mac

--- onpolicy/scripts/08-17-rel-formation-form-selfnav10-train-mpe-obs0-poly5-R.sh
+++ onpolicy/scripts/08-17-rel-formation-form-selfnav10-train-mpe-obs0-poly5-R.sh
@@ -5,7 +5,7 @@ num_landmarks=1
 num_agents=5
 algo="rmappo"
 exp="08-17-rel-formation-form-selfnav10-train-mpe-obs0-poly5-R"
-seed_max=1
+seed_max=2
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 let "seed=$seed+1"
