ed9545bd191094fbe5739a88c64e71dc044672ba
Yuzi Yan <yanyuzithu@163.com>
2021-08-18 22:59:00 +0800

    add some code from Mac

--- onpolicy/scripts/08-17-rel-formation-form-selfnav10-train-mpe-obs0-3600-newobs-R.sh
+++ onpolicy/scripts/08-17-rel-formation-form-selfnav10-train-mpe-obs0-3600-newobs-R.sh
@@ -5,7 +5,7 @@ num_landmarks=1
 num_agents=4
 algo="rmappo"
 exp="08-17-rel-formation-form-selfnav10-train-mpe-obs0-3600-newobs-R"
-seed_max=5
+seed_max=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 for seed in `seq ${seed_max}`;
