88ffd2be7fa6388b4cb6d06b17ff531e3cf6e300
Yuzi Yan <yanyuzithu@163.com>
2021-08-19 22:48:56 +0800

    add some code from Mac

--- onpolicy/scripts/08-18-dead-agent-R.sh
+++ onpolicy/scripts/08-18-dead-agent-R.sh
@@ -5,7 +5,7 @@ num_landmarks=1
 num_agents=5
 algo="rmappo"
 exp="08-18-dead-agent-R"
-seed=5
+seed=6
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 let "seed=$seed+1"
