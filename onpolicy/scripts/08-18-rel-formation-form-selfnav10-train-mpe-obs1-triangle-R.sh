5303847621d30d6d4fc5fd8c7eaead173e67cce8
Yuzi Yan <yanyuzithu@163.com>
2021-08-19 10:32:13 +0800

    add some code from Mac

--- onpolicy/scripts/08-18-rel-formation-form-selfnav10-train-mpe-obs1-triangle-R.sh
+++ onpolicy/scripts/08-18-rel-formation-form-selfnav10-train-mpe-obs1-triangle-R.sh
@@ -5,7 +5,7 @@ num_landmarks=1
 num_agents=3
 algo="rmappo"
 exp="08-18-rel-formation-form-selfnav10-train-mpe-obs1-triangle-R"
-seed=1
+seed=3
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 let "seed=$seed+1"
@@ -27,7 +27,7 @@ CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
 --num_static_obs 10 \
 --use_render \
 --episode_length 250 \
---render_episodes 5 \
+--render_episodes 1 \
 --num_static_obs 0 \
 --model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-18-rel-formation-form-selfnav10-train-mpe-obs1-triangle/run1/models" \
 --save_gifs \
