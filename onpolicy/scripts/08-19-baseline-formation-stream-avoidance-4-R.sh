7de187914ff478d52ea946570a1b7d2d4e660b41
Yuzi Yan <yanyuzithu@163.com>
2021-08-20 18:22:02 +0800

    add some code from Mac

--- onpolicy/scripts/08-19-baseline-formation-stream-avoidance-4-R.sh
+++ onpolicy/scripts/08-19-baseline-formation-stream-avoidance-4-R.sh
@@ -29,7 +29,7 @@ CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
 --episode_length 400 \
 --render_episodes 1 \
 --num_static_obs 0 \
---model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-18-rel-formation-form-selfnav10-train-mpe-obs1-triangle/run1/models" \
+--model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/formation_stream_avoidance_4/rmappo/08-19-baseline-formation-stream-avoidance-4/run1/models" \
 --save_gifs \
 --eval_interval 250 \
 --map-max-size 3600 \
