81ed31314b61768b189cb02dd16a57d189fa932b
Yuzi Yan <yanyuzithu@163.com>
2021-08-20 11:22:34 +0800

    add some code from Mac

--- onpolicy/envs/mpe/environment_formation_tracking.py
+++ onpolicy/envs/mpe/environment_formation_tracking.py
@@ -2,7 +2,7 @@ import gym
 from gym import spaces
 from gym.envs.registration import EnvSpec
 import numpy as np
-from multiagent.multi_discrete import MultiDiscrete
+from .multi_discrete import MultiDiscrete
 
 import pyglet
 from pyglet import gl
