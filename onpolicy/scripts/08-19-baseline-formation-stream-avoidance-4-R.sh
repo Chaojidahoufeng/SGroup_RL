9c083e1602a2e0bef377cd1b2d1792b77ab29c67
Yuzi Yan <yanyuzithu@163.com>
2021-08-20 18:21:11 +0800

    add some code from Mac

--- onpolicy/scripts/08-19-baseline-formation-stream-avoidance-4-R.sh
+++ onpolicy/scripts/08-19-baseline-formation-stream-avoidance-4-R.sh
@@ -5,7 +5,7 @@ num_landmarks=1
 num_agents=5
 algo="rmappo"
 exp="08-19-baseline-formation-stream-avoidance-4-R"
-seed=12
+seed=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 let "seed=$seed+1"
