9b372dc1427d956f2f91f0b1df3532dda5f03994
Yuzi Yan <yanyuzithu@163.com>
2021-08-20 18:21:04 +0800

    add some code from Mac

--- onpolicy/scripts/08-19-baseline-formation-stream-avoidance-4-R.sh
+++ onpolicy/scripts/08-19-baseline-formation-stream-avoidance-4-R.sh
@@ -4,38 +4,34 @@ scenario="formation_stream_avoidance_4"
 num_landmarks=1
 num_agents=5
 algo="rmappo"
-exp="08-19-baseline-formation-stream-avoidance-4"
-seed_max=1
+exp="08-19-baseline-formation-stream-avoidance-4-R"
+seed=12
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
-for seed in `seq ${seed_max}`;
-do
-    let "seed=$seed+1"
-    echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_mpe.py \
-    --env_name ${env} \
-    --algorithm_name ${algo} \
-    --experiment_name ${exp} \
-    --scenario_name ${scenario} \
-    --num_agents ${num_agents} \
-    --num_landmarks ${num_landmarks} \
-    --seed 50 \
-    --n_training_threads 1 \
-    --n_rollout_threads 1 \
-    --num_mini_batch 1 \
-    --episode_length 250 \
-    --num_env_steps 10000000 \
-    --ppo_epoch 10 \
-    --gain 0.01 \
-    --lr 7e-4 \
-    --critic_lr 7e-4 \
-    --user_name "mapping" \
-    --avoid-rew-weight 5 \
-    --form-rew-weight 0.05 \
-    --nav-rew-weight 0 \
-    --self-nav-rew-weight 10 \
-    --num_static_obs 0 \
-    --map-max-size 3600 \
-    --static_obs_intensity 3e-6 \
-    --topo_type "square"
-done
+let "seed=$seed+1"
+echo "seed is ${seed}:"
+CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
+--env_name ${env} \
+--algorithm_name ${algo} \
+--experiment_name ${exp} \
+--scenario_name ${scenario} \
+--num_agents ${num_agents} \
+--num_landmarks ${num_landmarks} \
+--seed ${seed} \
+--n_training_threads 1 \
+--n_rollout_threads 1 \
+--avoid-rew-weight 5 \
+--form-rew-weight 0.05 \
+--nav-rew-weight 0 \
+--self-nav-rew-weight 10 \
+--num_static_obs 10 \
+--use_render \
+--episode_length 400 \
+--render_episodes 1 \
+--num_static_obs 0 \
+--model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-18-rel-formation-form-selfnav10-train-mpe-obs1-triangle/run1/models" \
+--save_gifs \
+--eval_interval 250 \
+--map-max-size 3600 \
+--static_obs_intensity 2e-6 \
+--topo_type "square"
