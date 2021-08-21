7b9a70f99e8ed01ced3c67d7a1e5f02266d52418
Yuzi Yan <yanyuzithu@163.com>
2021-08-21 20:28:35 +0800

    add some code from Mac

--- onpolicy/scripts/08-20-rel-formation-form-selfnav10-train-mpe-obs0-square-R.sh
+++ onpolicy/scripts/08-20-rel-formation-form-selfnav10-train-mpe-obs0-square-R.sh
@@ -33,6 +33,6 @@ CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
 --save_gifs \
 --eval_interval 250 \
 --map-max-size 3600 \
---static_obs_intensity 1e-6 \
+--static_obs_intensity 0e-6 \
 --topo_type "square" \
 --use_softmax_last
