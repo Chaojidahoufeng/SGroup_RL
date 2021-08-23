84bb946659a15b9a2b66dbd877782958a7f63dc7
Chaojidahoufeng2 <13146012600@163.com>
2021-08-23 15:41:49 +0800

    add some code from Mac

--- onpolicy/scripts/MVE-08-22-train-formation-R.sh
+++ onpolicy/scripts/MVE-08-22-train-formation-R.sh
@@ -29,6 +29,7 @@ python render/render_mve.py \
 --usegui \
 --num_agents ${num_agents} \
 --num_landmarks ${num_landmarks} \
---num_obstacles ${num_obstacles}
+--num_obstacles ${num_obstacles} \
+--ideal_side_len 5.0
 #--wandb_name "tartrl"
 #--use_wandb
