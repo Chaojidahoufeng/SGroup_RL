0ed6fa99dddce3f5a1a93a4e34fc22d429bb3da4
Chaojidahoufeng2 <13146012600@163.com>
2021-09-02 00:18:22 +0800

    add some code from Mac

--- onpolicy/runner/shared/mpe_runner_dead_render.py
+++ onpolicy/runner/shared/mpe_runner_dead_render.py
@@ -256,8 +256,8 @@ class MPERunner(Runner):
             episode_rewards = []
 
             self.episode_length_1 = 200
-            self.episode_length_2 = 150
-            self.episode_length_3 = 150
+            self.episode_length_2 = 120
+            self.episode_length_3 = 120
 
             from onpolicy.algorithms.r_mappo.r_mappo import R_MAPPO as TrainAlgo
             from onpolicy.algorithms.r_mappo.algorithm.rMAPPOPolicy import R_MAPPOPolicy as Policy
