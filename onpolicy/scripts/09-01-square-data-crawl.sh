5c5eae9647c0d9249c3a7fb26110ea8adad0bc34
Chaojidahoufeng2 <13146012600@163.com>
2021-09-01 22:32:10 +0800

    add some code from Mac

--- onpolicy/scripts/09-01-square-data-crawl.sh
+++ onpolicy/scripts/09-01-square-data-crawl.sh
@@ -2,9 +2,9 @@
 env="MPE"
 scenario="rel_formation_form_error" 
 num_landmarks=1
-num_agents=5
+num_agents=4
 algo="rmappo"
-exp="08-18-dead-agent-R"
+exp="09-01-square-data-crawl"
 seed=10
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
