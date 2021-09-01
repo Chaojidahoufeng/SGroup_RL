9d9a64a58c8381a375ba71d60a2eda5a3523e4e7
Chaojidahoufeng2 <13146012600@163.com>
2021-09-01 23:53:00 +0800

    add some code from Mac

--- onpolicy/runner/shared/mpe_runner_dead_render.py
+++ onpolicy/runner/shared/mpe_runner_dead_render.py
@@ -274,7 +274,7 @@ class MPERunner(Runner):
 
             self.trainer = TrainAlgo(self.all_args, self.policy, device = self.device)
 
-            self.model_dir = "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-18-rel-formation-form-selfnav10-train-mpe-obs1-poly5/run1/models"
+            self.model_dir = "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-26-rel-formation-form-selfnav10-train-mpe-obs0-poly5/run2/models"
             self.restore()
 
             
@@ -349,7 +349,7 @@ class MPERunner(Runner):
 
             self.trainer = TrainAlgo(self.all_args, self.policy, device = self.device)
 
-            self.model_dir = "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-17-rel-formation-form-selfnav10-train-mpe-obs0-3600-newobs/run1/models"
+            self.model_dir = "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-26-rel-formation-form-selfnav10-train-mpe-obs0-square/run1/models"
             self.restore()
 
             obs, rewards, dones, infos = envs.step(actions_env)
@@ -411,7 +411,7 @@ class MPERunner(Runner):
 
             self.trainer = TrainAlgo(self.all_args, self.policy, device = self.device)
 
-            self.model_dir = "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-17-rel-formation-form-selfnav10-train-mpe-obs0-triangle/run1/models"
+            self.model_dir = "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-26-rel-formation-form-selfnav10-train-mpe-obs0-triangle/run1/models"
             self.restore()
 
             obs, rewards, dones, infos = envs.step(actions_env)
--- onpolicy/scripts/09-01-dead-agent-R.sh
+++ onpolicy/scripts/09-01-dead-agent-R.sh
@@ -4,10 +4,11 @@ scenario="rel_formation_form_error"
 num_landmarks=1
 num_agents=5
 algo="rmappo"
-exp="08-26-rel-formation-form-selfnav10-train-mpe-obs0-poly5-R"
-seed=5
+exp="09-01-dead-agent-R"
+seed=10
 
-echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed}"
+echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
+let "seed=$seed+1"
 echo "seed is ${seed}:"
 CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
 --env_name ${env} \
@@ -24,15 +25,15 @@ CUDA_VISIBLE_DEVICES=0 python render/render_mpe.py \
 --nav-rew-weight 0 \
 --self-nav-rew-weight 10 \
 --num_static_obs 10 \
---episode_length 350 \
+--use_render \
+--episode_length 250 \
 --render_episodes 1 \
 --num_static_obs 0 \
---model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-26-rel-formation-form-selfnav10-train-mpe-obs0-poly5/run2/models" \
+--model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MPE/rel_formation_form_error/rmappo/08-18-rel-formation-form-selfnav10-train-mpe-obs1-poly5/run1/models" \
 --save_gifs \
---use_render \
 --eval_interval 250 \
 --map-max-size 3600 \
 --static_obs_intensity 0e-6 \
 --topo_type "square" \
---use_softmax_last #\
-#--use_render
+--use_softmax_last \
+--showing_mode "render_dead"
