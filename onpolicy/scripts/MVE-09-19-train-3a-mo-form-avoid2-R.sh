9e468f9e72fd78304b75f2e7636d34cc9dc7eabf
Chaojidahoufeng2 <13146012600@163.com>
2021-09-20 09:36:48 +0800

    add some code from Mac

--- onpolicy/scripts/MVE-09-19-train-3a-mo-form-avoid2-R.sh
+++ onpolicy/scripts/MVE-09-19-train-3a-mo-form-avoid2-R.sh
@@ -23,7 +23,7 @@ python render/render_mve.py \
 --use_render \
 --episode_length 25 \
 --render_episodes 5 \
---model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MVE/yyz3-avoid/mappo/MVE-09-19-train-3a-mo-form-avoid2/run5/models" \
+--model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MVE/yyz3-avoid/mappo/MVE-09-19-train-3a-mo-form-avoid2/run2/models" \
 --use_recurrent_policy \
 --usegui \
 --num_agents ${num_agents} \
