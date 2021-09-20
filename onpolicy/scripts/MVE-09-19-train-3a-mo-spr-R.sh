59ba7f05e048b417ea516b2606cede05224eb7fb
Chaojidahoufeng2 <13146012600@163.com>
2021-09-20 09:23:19 +0800

    add some code from Mac

--- onpolicy/scripts/MVE-09-19-train-3a-mo-spr-R.sh
+++ onpolicy/scripts/MVE-09-19-train-3a-mo-spr-R.sh
@@ -23,7 +23,7 @@ python render/render_mve.py \
 --use_render \
 --episode_length 25 \
 --render_episodes 5 \
---model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MVE/yyz3/mappo/MVE-09-13-train-yyz3-2_0-1/run1/models" \
+--model_dir "/home/yanyz/yanyz/gitlab/onpolicy/onpolicy/scripts/results/MVE/yyz3/mappo/MVE-09-19-train-4a-mo-spr/run1/models" \
 --use_recurrent_policy \
 --usegui \
 --num_agents ${num_agents} \
